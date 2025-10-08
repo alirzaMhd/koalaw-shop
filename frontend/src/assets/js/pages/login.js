b8f01d4ec5332c2653247a1f2dff09abb8035c64
alirzaMhd <71058261+alirzaMhd@users.noreply.github.com>
2025-10-08 13:25:38 +0330

    fix login

--- frontend/src/assets/js/pages/login.js
+++ frontend/src/assets/js/pages/login.js
@@ -110,8 +110,6 @@
           if (data.data?.user) {
             localStorage.setItem("user", JSON.stringify(data.data.user));
           }
-          console.log("🍪 Document cookies:", document.cookie);
-          console.log("📦 Response data:", data);
           window.location.href = "/profile";
           return;
         }
